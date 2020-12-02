53000513d0c58478d62869fad8e25e80695c7487
robertaassis <71094987+robertaassis@users.noreply.github.com>
2020-12-02 19:33:39 -0300

    Update Cliente.java

--- src/Lib/Cliente.java
+++ src/Lib/Cliente.java
@@ -5,4 +5,43 @@ public class Cliente {
     private int id;
     private int livrosEmprestados;
     private int livrosReservados;
+
+    public Cliente(String nome, int id, int livrosEmprestados, int livrosReservados) {
+        this.nome = nome;
+        this.id = id;
+        this.livrosEmprestados = livrosEmprestados;
+        this.livrosReservados = livrosReservados;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLivrosEmprestados() {
+        return livrosEmprestados;
+    }
+
+    public void setLivrosEmprestados(int livrosEmprestados) {
+        this.livrosEmprestados = livrosEmprestados;
+    }
+
+    public int getLivrosReservados() {
+        return livrosReservados;
+    }
+
+    public void setLivrosReservados(int livrosReservados) {
+        this.livrosReservados = livrosReservados;
+    }
 }
